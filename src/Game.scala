91b2dd7da7713f955aa1c5cfb454e3b77406f802
Surya Suravarapu <surya@new-host-3.home>
2010-03-07 21:53:11 -0500

    Near complete, may be?

--- src/Game.scala
+++ src/Game.scala
@@ -7,42 +7,67 @@ object Game {
 
   def main(args: Array[String]) {
 
-    val board = Array(Array("-", "-", "-", "-", "-"),
-      Array("-", "-", "-", "-", "-"),
-      Array("-", "*", "*", "*", "-"),
-      Array("-", "-", "-", "-", "-"),
-      Array("-", "-", "-", "-", "-"))
+//    val board = Array(
+//      Array("-", "-", "-", "-", "-"),
+//      Array("-", "-", "-", "-", "-"),
+//      Array("-", "*", "*", "*", "-"),
+//      Array("-", "-", "-", "-", "-"),
+//      Array("-", "-", "-", "-", "-"))
 
-    board.foreach {i =>
-      i.foreach { j =>
-        print (j)  
-      }
-      println
-    }
+    val board = Array(
+      Array("-", "-", "-", "-", "-", "-"),
+      Array("-", "*", "*", "-", "-", "-"),
+      Array("-", "*", "-", "-", "-", "-"),
+      Array("-", "-", "-", "-", "*", "-"),
+      Array("-", "-", "-", "*", "*", "-"),
+      Array("-", "-", "-", "-", "-", "-"))
+        
+    printBoard(board)
 
-    for (i <- 0 until board.length) {
-      for (j <- 0 until board.length) {
-        println(computeLives(board, i, j))
+    for (loops <- 1 to 5) {
+      for (i <- 0 until board.length) {
+        for (j <- 0 until board.length) {
+          val lives = computeLives(board, i, j)
+          val cell = board (i)(j) match {
+            case "-" => if (lives == 3) "*"
+              else "-"
+            case "*" => if (lives <= 1 || lives >= 4) "-"
+              else if (lives == 2 || lives == 3) "*"
+              else "*"
+          }
+          board(i)(j) = cell
+        }
       }
+      printBoard(board)
     }
   }
 
   def computeLives(board:Array[Array[String]], currentRow:Int, currentColumn:Int):Int = {
     var lives:Int = 0
-    println
-    println("(" + currentRow + ", " + currentColumn + ")")
+    //println
+    //println("(" + currentRow + ", " + currentColumn + ")")
     for (i <- Math.max(0, currentRow - 1) to Math.min(board.length - 1, currentRow + 1)) {
       for (j <- Math.max(0, currentColumn - 1) to Math.min(board.length - 1, currentColumn + 1)) {
         if (j == currentColumn && i == currentRow)
-          print("!")
+          lives
         else {
-          print (board(i)(j))
+          //print (board(i)(j))
           if (board(i)(j) == "*")
             lives = lives + 1
         }
       }
-      println
+      //println
     }
     lives
+  }
+
+  def printBoard(board:Array[Array[String]]) = {
+    println
+    board.foreach {i =>
+      i.foreach { j =>
+        print (j)
+      }
+      println
+    }
   }
 }
