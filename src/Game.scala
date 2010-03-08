43f88504b4efc3661e838daa6b8b0addefa8cbb0
Surya Suravarapu <surya@new-host-3.home>
2010-03-08 18:24:10 -0500

    Cleanup

--- src/Game.scala
+++ src/Game.scala
@@ -7,13 +7,6 @@ object Game {
 
   def main(args: Array[String]) {
 
-//    val board = Array(
-//      Array("-", "-", "-", "-", "-"),
-//      Array("-", "-", "-", "-", "-"),
-//      Array("-", "*", "*", "*", "-"),
-//      Array("-", "-", "-", "-", "-"),
-//      Array("-", "-", "-", "-", "-"))
-
     val board = Array(
       Array("-", "-", "-", "-", "-", "-"),
       Array("-", "*", "*", "-", "-", "-"),
@@ -24,20 +17,21 @@ object Game {
         
     printBoard(board)
 
-    for (loops <- 1 to 2) {
+    for (period <- 1 to 2) {
+      val changes = scala.collection.mutable.Map.empty[Tuple2[Int, Int], String]
       for (i <- 0 until board.length) {
         for (j <- 0 until board.length) {
           val lives = computeLives(board, i, j)
           val cell = board (i)(j) match {
             case "-" => if (lives == 3) "*"
-              else "-"
+              else board(i)(j)
             case "*" => if (lives <= 1 || lives >= 4) "-"
-              else if (lives == 2 || lives == 3) "*"
-              else "*"
+              else board(i)(j)
           }
-          board(i)(j) = cell
+          changes + ((i, j) -> cell)
         }
       }
+      changes foreach (m => board(m._1._1)(m._1._2) = m._2)
       printBoard(board)
     }
   }
